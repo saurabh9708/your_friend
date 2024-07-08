332cbd0c91fe518596db346bec6d79e917eac830
saurabh9708 <guptashivam748@gmail.com>
2024-07-08 21:27:55 +0530

    Majorupgrade

--- lib/main.dart
+++ lib/main.dart
@@ -41,16 +41,17 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            appBarTheme: AppBarTheme(
-          // centerTitle: true,
-          elevation: 1,
-          backgroundColor: Colors.blue[200],
-        )),
-        // home: SplashScreen(),
-        // home: LoginScreen(),
-        home: HomeScreen());
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+        // centerTitle: true,
+        elevation: 1,
+        backgroundColor: Colors.blue[200],
+      )),
+      home: SplashScreen(),
+      // home: LoginScreen(),
+      // home: HomeScreen()
+    );
   }
 }
